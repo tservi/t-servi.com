847f387957ccfc9461b65129badd54818d57dc42
tservi <jeantinguelyawais@gmail.com>
2016-12-13 17:34:25 +0100

    minimal index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php /*used to display posts */ ?>
 <?php /*used to display page */ ?>
-<?php /*displayed only if home.php does not exist */ ?>
+<?php /*displayed only if files does not exist */ ?>
+<?php
+    get_header();
+    if ( have_posts() ) : while ( have_posts() ) : the_post();
+        the_content();
+    endwhile;
+    else :
+        _e( 'Sorry, no posts matched your criteria.', 'textdomain' );
+    endif;
+    get_footer(); 
+?>
